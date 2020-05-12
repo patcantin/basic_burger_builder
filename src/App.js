5a8d29d2a84ef937b4a4cae1ebe128956c88e87e
patcantin <patcantin23@gmail.com>
2020-05-12 12:59:13 -0400

    created containers/BurgerBuilder + Layout/Layout.scss files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 
 function App() {
   return (
     <div>
       <Layout>
-        <h1>Test</h1>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
