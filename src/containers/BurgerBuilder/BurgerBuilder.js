4c7733fbe2687ab7b0356c6e1a5bb7e02d49e54e
patcantin <patcantin23@gmail.com>
2020-05-13 11:01:53 -0400

    implemented function in BurgerBuilder that render ingredients graphically if buttons are pressed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,12 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 1,
+  bacon: 0.7,
+  meat: 1.3
+}
 
 
 class BurgerBuilder extends Component {
@@ -15,16 +21,40 @@ class BurgerBuilder extends Component {
         salad: 0,
         bacon: 0,
         cheese: 0,
-        meat: 3
-      }
+        meat: 0
+      },
+      totalPrice: 4
     }
   }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice
+    })
+  };
+
+  removeIngredientHandler = (type) => {
+
+  };
+
+
   render() {
     return(
       <Aux>
         <Burger ingredients={this.state.ingredients}/>
         <div>
-          <BuildControls />
+          <BuildControls
+            ingredientAdded={this.addIngredientHandler} />
         </div>
       </Aux>
     );
