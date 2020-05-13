7043b582427781a2f98882fc58b374eff30d01cb
patcantin <patcantin23@gmail.com>
2020-05-13 09:35:26 -0400

    input Please start adding ingredients in graphic burger if no added ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,24 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.scss';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients) //return an array of keys [salad, bacon, cheese, meat]
+  let transformedIngredients = Object.keys(props.ingredients) //return an array of keys [salad, bacon, cheese, meat]
       .map(igKey => { //mapping an array for each array items above
         return [...Array(props.ingredients[igKey])].map((_, i) => { //[Array(1), Array(1), Array(2), Array(4)]
           return <BurgerIngredient key={igKey + i} type={igKey} />
           // each array content Objects, 2 if 2 meat including key and type [{}, {}}
         });
-      });
-      //console.log(transformedIngredients);
+      })
+      .reduce((arr, el) => {
+        return arr.concat(el)
+        // returns the length of transformed ingrediens arry by counting/concat his number of objects
+      }, []);
+
+      console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) { // if transformedIngredients empty
+      transformedIngredients = <p>Please start adding ingredients</p>
+  }
+
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
