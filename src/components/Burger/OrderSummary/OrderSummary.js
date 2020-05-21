1153264f902b97b7ac2e7fe7cc8ef6f5b2fee82b
patcantin <patcantin23@gmail.com>
2020-05-21 14:43:49 -0400

    implementation of shouldComponentUpdate / componentDidUpdate in Modal component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UserInterface/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients) // [salad, meat, bacon, ect....]
-      .map(igKey => { // for each ingredients, i return his name {igKey} + number of ingredients {props.ingredients[igKey]}
+class OrderSummary extends Component {
+  // component turned to a class component only to use componentDidUpdate
+  componentDidUpdate() {
+    console.log('[OrderSummary] DidUpdate');
+  };
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+
+
+    }
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients) // [salad, meat, bacon, ect....]
+      .map(igKey => { // for each ingredients, i return his name {igKey} + number of ingredients {this.props.ingredients[igKey]}
         return (
           <li key={igKey}>
-            <span>{igKey}</span> : {props.ingredients[igKey]}
+            <span>{igKey}</span> : {this.props.ingredients[igKey]}
           </li>
         );
       });
 
-console.log(ingredientSummary);
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Ingredints of your burger:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <h3>Total: {props.price.toFixed(2)}$</h3>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.cancel}>Cancel</Button>
-      <Button btnType="Success" clicked={props.continue}>Buy</Button>
-    </Aux>
-  )
+    console.log(ingredientSummary);
 
-};
+    return(
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Ingredints of your burger:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <h3>Total: {this.props.price.toFixed(2)}$</h3>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.cancel}>Cancel</Button>
+        <Button btnType="Success" clicked={this.props.continue}>Buy</Button>
+      </Aux>
+    );
+  }
+}
 
-export default orderSummary;
+
+export default OrderSummary;
